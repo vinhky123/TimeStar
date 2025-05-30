b6fbfe1ccd2c90bb49f1111f0b3a3e24ec332432
Vĩnh Kỳyy <89082148+vinhky123@users.noreply.github.com>
2025-05-30 12:17:04 +0700

    Update TimeStar8.py

--- models/TimeStar8.py
+++ models/TimeStar8.py
@@ -7,13 +7,12 @@ import numpy as np
 
 
 class FlattenHead(nn.Module):
-    def __init__(self, n_vars, nf, target_window, head_dropout=0, J=1):
+    def __init__(self, n_vars, nf, target_window, head_dropout=0):
         super().__init__()
         self.n_vars = n_vars
         self.flatten = nn.Flatten(start_dim=-2)
         self.linear = nn.Linear(nf, target_window)
         self.dropout = nn.Dropout(head_dropout)
-        self.J = J
 
     def forward(self, x):  # x: [bs x nvars x d_model x patch_num]
         x = self.flatten(x)
@@ -23,15 +22,13 @@ class FlattenHead(nn.Module):
 
 
 class EnEmbedding(nn.Module):
-    def __init__(self, n_vars, d_model, patch_len, dropout, J):
+    def __init__(self, n_vars, d_model, patch_len, dropout):
         super(EnEmbedding, self).__init__()
         # Patching
         self.patch_len = patch_len
-        self.d_model = d_model
-        self.J = J
 
         self.value_embedding = nn.Linear(patch_len, d_model, bias=False)
-        self.glb_token = nn.Parameter(torch.randn(1, n_vars, 1, d_model * J))
+        self.glb_token = nn.Parameter(torch.randn(1, n_vars, 1, d_model))
         self.position_embedding = PositionalEmbedding(d_model)
 
         self.dropout = nn.Dropout(dropout)
@@ -46,8 +43,7 @@ class EnEmbedding(nn.Module):
         # Input encoding
         x = self.value_embedding(x) + self.position_embedding(x)
         x = torch.reshape(x, (-1, n_vars, x.shape[-2], x.shape[-1]))
-        glb_subtokens = glb.view(x.shape[0], n_vars, self.J, self.d_model)
-        x = torch.cat([x, glb_subtokens], dim=2)
+        x = torch.cat([x, glb], dim=2)
         x = torch.reshape(x, (x.shape[0] * x.shape[1], x.shape[2], x.shape[3]))
         return self.dropout(x), n_vars
 
@@ -82,8 +78,7 @@ class EncoderLayer(nn.Module):
         d_ff=None,
         dropout=0.1,
         activation="relu",
-        J=1,
-        patch_num=None,
+        n_vars=None,
     ):
         super(EncoderLayer, self).__init__()
         d_ff = d_ff or 4 * d_model
@@ -95,10 +90,8 @@ class EncoderLayer(nn.Module):
         self.norm2 = nn.LayerNorm(d_model)
         self.norm3 = nn.LayerNorm(d_model)
         self.dropout = nn.Dropout(dropout)
-        self.patch_num = patch_num
         self.activation = F.relu if activation == "relu" else F.gelu
-        self.glb_proj = nn.Linear(d_model * J, d_model)
-        self.J = J
+        self.refine = nn.Parameter(torch.randn(1, n_vars, d_model))
 
     def forward(self, x, cross, x_mask=None, cross_mask=None, tau=None, delta=None):
         B, L, D = cross.shape
@@ -106,22 +99,20 @@ class EncoderLayer(nn.Module):
             self.self_attention(x, x, x, attn_mask=x_mask, tau=tau, delta=None)[0]
         )
         x = self.norm1(x)
+        self.refine = self.refine.to(x.device)
+        self.refine = self.refine.repeat((B, 1, 1))
 
-        x_glb_ori = x[:, -self.J :, :]
-        x_glb_ori = x_glb_ori.mean(dim=1, keepdim=True)
-
+        x_glb_ori = x[:, -1, :].unsqueeze(1)
+        x_glb_ori = x_glb_ori * self.refine
         x_glb = torch.reshape(x_glb_ori, (B, -1, D))
         x_glb_attn = self.dropout(
             self.cross_attention(
                 x_glb, cross, cross, attn_mask=cross_mask, tau=tau, delta=delta
             )[0]
         )
-
         x_glb_attn = torch.reshape(
-            x_glb_attn,
-            (x_glb_attn.shape[0] * x_glb_attn.shape[1], x_glb_attn.shape[2]),
+            x_glb_attn, (x_glb_attn.shape[0] * x_glb_attn.shape[1], x_glb_attn.shape[2])
         ).unsqueeze(1)
-
         x_glb = x_glb_ori + x_glb_attn
         x_glb = self.norm2(x_glb)
 
@@ -145,14 +136,9 @@ class Model(nn.Module):
         self.patch_len = configs.patch_len
         self.patch_num = int(configs.seq_len // configs.patch_len)
         self.n_vars = 1 if configs.features == "MS" else configs.enc_in
-        self.J = configs.J
         # Embedding
         self.en_embedding = EnEmbedding(
-            self.n_vars,
-            configs.d_model,
-            self.patch_len,
-            configs.dropout,
-            self.J,
+            self.n_vars, configs.d_model, self.patch_len, configs.dropout
         )
 
         self.ex_embedding = DataEmbedding_inverted(
@@ -191,17 +177,54 @@ class Model(nn.Module):
                     configs.d_ff,
                     dropout=configs.dropout,
                     activation=configs.activation,
-                    J=self.J,
-                    patch_num=self.patch_num,
+                    n_vars=configs.enc_in,
                 )
                 for l in range(configs.e_layers)
             ],
             norm_layer=torch.nn.LayerNorm(configs.d_model),
         )
-        self.head_nf = configs.d_model * (self.patch_num + self.J)
+        self.head_nf = configs.d_model * (self.patch_num + 1)
         self.head = FlattenHead(
             configs.enc_in, self.head_nf, configs.pred_len, head_dropout=configs.dropout
         )
+
+    def forecast(self, x_enc, x_mark_enc, x_dec, x_mark_dec):
+        if self.use_norm:
+            # Normalization from Non-stationary Transformer
+            means = x_enc.mean(1, keepdim=True).detach()
+            x_enc = x_enc - means
+            stdev = torch.sqrt(
+                torch.var(x_enc, dim=1, keepdim=True, unbiased=False) + 1e-5
+            )
+            x_enc /= stdev
+
+        _, _, N = x_enc.shape
+
+        en_embed, n_vars = self.en_embedding(
+            x_enc[:, :, -1].unsqueeze(-1).permute(0, 2, 1)
+        )
+        ex_embed = self.ex_embedding(x_enc[:, :, :-1], x_mark_enc)
+
+        enc_out = self.encoder(en_embed, ex_embed)
+        enc_out = torch.reshape(
+            enc_out, (-1, n_vars, enc_out.shape[-2], enc_out.shape[-1])
+        )
+        # z: [bs x nvars x d_model x patch_num]
+        enc_out = enc_out.permute(0, 1, 3, 2)
+
+        dec_out = self.head(enc_out)  # z: [bs x nvars x target_window]
+        dec_out = dec_out.permute(0, 2, 1)
+
+        if self.use_norm:
+            # De-Normalization from Non-stationary Transformer
+            dec_out = dec_out * (
+                stdev[:, 0, -1:].unsqueeze(1).repeat(1, self.pred_len, 1)
+            )
+            dec_out = dec_out + (
+                means[:, 0, -1:].unsqueeze(1).repeat(1, self.pred_len, 1)
+            )
+
+        return dec_out
 
     def forecast_multi(self, x_enc, x_mark_enc, x_dec, x_mark_dec):
         if self.use_norm:
