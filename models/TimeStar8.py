d072cea8a0fc30ab367260cd00049841c53f12ac
Vĩnh Kỳyy <89082148+vinhky123@users.noreply.github.com>
2025-05-30 09:41:27 +0700

    Update TimeStar8.py

--- models/TimeStar8.py
+++ models/TimeStar8.py
@@ -7,12 +7,13 @@ import numpy as np
 
 
 class FlattenHead(nn.Module):
-    def __init__(self, n_vars, nf, target_window, head_dropout=0):
+    def __init__(self, n_vars, nf, target_window, head_dropout=0, J=1):
         super().__init__()
         self.n_vars = n_vars
         self.flatten = nn.Flatten(start_dim=-2)
         self.linear = nn.Linear(nf, target_window)
         self.dropout = nn.Dropout(head_dropout)
+        self.J = J
 
     def forward(self, x):  # x: [bs x nvars x d_model x patch_num]
         x = self.flatten(x)
@@ -192,7 +193,7 @@ class Model(nn.Module):
             ],
             norm_layer=torch.nn.LayerNorm(configs.d_model),
         )
-        self.head_nf = configs.d_model * (self.patch_num + 1)
+        self.head_nf = configs.d_model * (self.patch_num + self.J)
         self.head = FlattenHead(
             configs.enc_in, self.head_nf, configs.pred_len, head_dropout=configs.dropout
         )
